df6a4f3aa262fbb9005cbb009ef4b02bb28551e0
wratterman <will.ratterman@gmail.com>
2017-10-23 10:57:20 -0600

    Post working

--- src/lib/paragraph-listener.js
+++ src/lib/paragraph-listener.js
@@ -1,30 +1,55 @@
 const $ = require('jquery')
+const ErrorHelper = require('./error-helper')
+const baseUrl = "https://wordwatch-api.herokuapp.com/api/v1"
 
 const sortWords = () => {
   $("button").on("click", function(element) {
-    a = (this.parentElement.children[1].value).split(" ")
-    timesSaid = {}
-    a.forEach(function(word){
-      if (timesSaid[word.toLowerCase()]) {
-        timesSaid[word.toLowerCase()] ++
-      } else {
-        timesSaid[word.toLowerCase()] = 1
-      }
-    })
-    let sorted = []
-    for (var word in timesSaid) {
-      sorted.push([word, timesSaid[word]])
+    words = (this.parentElement.children[1].value).split(" ")
+    parseParaphraph(words)
+  })
+}
+
+const parseParaphraph = (words) => {
+  let timesSaid = {}
+  words.forEach(function(word){
+    if (timesSaid[word.toLowerCase()]) {
+      timesSaid[word.toLowerCase()] ++
+    } else {
+      timesSaid[word.toLowerCase()] = 1
     }
-    sorted.sort(function(a, b) {
-      return a[1] - b[1]
-    })
-    let display = []
-    sorted.reverse().forEach(function(word) {
-      display.push(word[0])
-      $(".word-count").append(word[0] + "<br>")
-    })
-    display.forEach(function(word){
-    })
+  })
+  formatAndSortVals(timesSaid)
+}
+
+const formatAndSortVals = (timesSaid) => {
+  let sorted = []
+  for (var word in timesSaid) {
+    sorted.push([word, timesSaid[word]])
+  }
+  sorted.sort(function(a, b) {
+    return a[1] - b[1]
+  })
+  appendPage(sorted)
+}
+
+const appendPage = (sorted) => {
+  sorted.reverse().forEach(function(word) {
+    $(".word-count").append(word[0] + "<br>")
+    postWord(word[0])
+  })
+}
+
+const postWord = (word) => {
+  $.ajax({
+    method: "POST",
+    url: `${baseUrl}/words`,
+    data: { word: {value: `${word}` } },
+    success: function (data){
+      console.log("nice")
+    },
+    failure: function(){
+      ErrorHelper.showError("Could Not Load")
+    }
   })
 }
 
--- src/lib/top-word-listener.js
+++ src/lib/top-word-listener.js
@@ -1,11 +1,11 @@
 const $ = require('jquery')
 const ErrorHelper = require('./error-helper')
-const baseUrl = "https://wordwatch-api.herokuapp.com"
+const baseUrl = "https://wordwatch-api.herokuapp.com/api/v1"
 
 const getWord = () => {
   $.ajax({
     method: "GET",
-    url: baseUrl + "/api/v1/top_word",
+    url: `${baseUrl}/top_word`,
     success: function (data){
       a = JSON.stringify(data.word)
       $("h3").append(a)
